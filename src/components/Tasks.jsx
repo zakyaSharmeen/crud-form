d3729a54db1f546c66696dc29eb88ff79b2152b5
“zakya” <zakyasharmeen@gmail.com>
2024-03-05 13:53:04 +0530

    first commit

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+export default function Tasks() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addBtn = () => {
+    if (inputValue.trim() !== "") {
+      setItems([...items, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const removeItem = (index) => {
+    const updateItem = [...items];
+    updateItem.splice(index, 1);
+    setItems(updateItem);
+  };
+  const updateItem = (index, newValue) => {
+    const updateItem = [...items];
+    updateItem[index] = newValue;
+    setItems(updateItem);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addBtn}>Add</button>
+      <ul>
+        {items.map((data, index) => (
+          <li key={index}>
+            {data}
+            <button onClick={() => removeItem(index)}>Delete</button>
+            <button onClick={() => updateItem(index, prompt("update a task"))}>
+              Update
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
